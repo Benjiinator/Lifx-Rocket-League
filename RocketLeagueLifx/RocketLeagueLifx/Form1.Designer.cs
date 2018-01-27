5779b2f6d39675a57d1e91a5f03cd966aafdf44a
Benjiinator <35868614+Benjiinator@users.noreply.github.com>
2018-01-27 15:06:59 +0100

    Upload of the project from visual studio
    Uploading of the LifxRocketLeague project.

---
+++ RocketLeagueLifx/RocketLeagueLifx/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace RocketLeagueLifx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rocket League pID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "#";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Process Status:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(120, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Closed";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Orange Goals:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(120, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Blue Goals:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(120, 55);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(180, 80);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "LifxRocketLeague";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ RocketLeagueLifx/RocketLeagueLifx/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace RocketLeagueLifx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Runs Background worker so that ui is responsive.
+            if (!backgroundWorker1.IsBusy)
+                backgroundWorker1.RunWorkerAsync();
+        }
+        // Creates a new Memory reader/writer object using a library called memory.dll which can be found here: https://github.com/erfg12/memory.dll/wiki
+        public Mem m = new Mem();
+        // Creates a new object which stores a your lifx token and a memory pointer to each goal.
+        public LifxSettings lifxSettings = new LifxSettings();
+        // Sets each goals value to -2(having it at 0 or - 1 causes problems if there is already 1 or 0 goals on either side).
+        public int BlueGoals = -2;
+        public int OrangeGoals = -2;
+        private void backgroundWorker1_DoWork_1(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                int pID = m.getProcIDFromName("RocketLeague"); // Get proc ID of game
+
+                bool openProc = false; //Is process open?
+
+                if (pID > 0) //Try opening process
+                {
+                    openProc = m.OpenProcess("RocketLeague");
+                    label2.Invoke((MethodInvoker)delegate
+                    {
+                        label2.Text = pID.ToString();
+                    });
+                } else
+                {
+                    label2.Invoke((MethodInvoker)delegate
+                    {
+                        label2.Text = "#";
+                    });
+                }
+
+                if (openProc) //Run if Rocket League is Open
+                {
+                    // Display in the ui of the program that Rocket League is Open.
+                    label4.Invoke((MethodInvoker)delegate
+                    {
+                        label4.Text = "Open";
+                        label4.ForeColor = Color.Green;
+                    });
+
+                    // Displays in the programs ui the amount of goals that orange currently have.
+                    label6.Invoke((MethodInvoker)delegate
+                    {
+                        label6.Text = m.readInt(lifxSettings.orangeGoalPointer).ToString();
+                    });
+                    // Displays in the programs ui the amount of goals that blue currently have.
+                    label8.Invoke((MethodInvoker)delegate
+                    {
+                        label8.Text = m.readInt(lifxSettings.blueGoalPointer).ToString();
+                    });
+
+                    //If amount of goals on blue team + 1 = memory reading of blue goals run this code (Runs if blue scores).
+                    if (BlueGoals + 1 == m.readInt(lifxSettings.blueGoalPointer))
+                    {
+                        // Sets blue color over 0.3 seconds
+                        Lifx("{\"brightness\": 0.80, \"color\": { \"hue\": 209, \"saturation\": 1, \"kelvin\": 2500, \"duration\": \"0.3\" } }");
+                        // Sets Brightsness over 0.5 Seconds
+                        Lifx("{\"brightness\": \"0.25\"}");
+                        // Goes back to a normal color over 10 seconds
+                        Lifx("{\"brightness\": 0.60, \"duration\": \"10.0\", \"color\": { \"hue\": 49, \"saturation\": 1, \"kelvin\": 2500 } }");
+                    }
+                    //If amount of goals on orange team + 1 = memory reading of blue goals run this code (Runs if orange scores).
+                    if (OrangeGoals + 1 == m.readInt(lifxSettings.orangeGoalPointer))
+                    {
+                        // Sets orange color over 0.3 seconds
+                        Lifx("{\"brightness\": 0.80, \"color\": { \"hue\": 35, \"saturation\": 1, \"kelvin\": 2500, \"duration\": \"0.3\" } }");
+                        // Sets Brightsness over 0.5 Seconds
+                        Lifx("{\"brightness\": \"0.25\"}");
+                        // Goes back to a normal color over 10 seconds
+                        Lifx("{\"brightness\": 0.60, \"duration\": \"10.0\", \"color\": { \"hue\": 49, \"saturation\": 1, \"kelvin\": 2500 } }");
+                    }
+
+                    //Set goal variables = memory reading of each goal.
+                    BlueGoals = m.readInt(lifxSettings.blueGoalPointer);
+                    OrangeGoals = m.readInt(lifxSettings.orangeGoalPointer);
+
+                } else // Runs if Rocket League is Closed.
+                {
+                    // Display in the ui of the program that Rocket League is Closed.
+                    label4.Invoke((MethodInvoker)delegate
+                    {
+                        label4.Text = "Closed";
+                        label4.ForeColor = Color.DarkRed;
+                    });
+                }
+            }
+        }
+        public void Lifx(string data)
+        {
+            if(lifxSettings.token != "")
+            {
+                WebClient client = new WebClient();
+                client.Headers.Add("authorization", "Bearer " + lifxSettings.token);
+                string url = "https://api.lifx.com/v1/lights/all/state";
+                client.UploadData(url, "PUT", Encoding.UTF8.GetBytes(data));
+            }
+        }
+        public class LifxSettings
+        {
+            // Your Lifx token which can be genereted here: https://cloud.lifx.com/sign_in
+            public string token = "";
+            // Pointer to each goal in the memory.
+            public string blueGoalPointer = "base+0x019CBB24,0xAC,0x294,0x4DC,0x0,0x20C";
+            public string orangeGoalPointer = "base+0x019CBB24,0xAC,0x294,0x4DC,0x4,0x20C";
+
+        }
+
+    }
+}
